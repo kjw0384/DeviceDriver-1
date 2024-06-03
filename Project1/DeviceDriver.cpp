9aa8eca188ad7f6191751695c8d4c3353461ceb0
DESKTOP-5FBE16T\User <kjw0384@gmail.com>
2024-06-03 09:35:56 +0900

    Read5회 수행 case - Green

--- Project1/DeviceDriver.cpp
+++ Project1/DeviceDriver.cpp
@@ -6,7 +6,17 @@ DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 int DeviceDriver::read(long address)
 {
     // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    int nReturn = 0;
+    int nReadValue = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        nReadValue = (int)(m_hardware->read(address));
+        if (i == 0)
+            nReturn = nReadValue;
+        else if (nReturn != nReadValue)
+            return -1;
+    }
+    return nReturn;
 }
 
 void DeviceDriver::write(long address, int data)
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,6 +1,29 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
+#include "../Project1/DeviceDriver.cpp"
+#include "../Project1/FlashMemoryDevice.h"
 
-TEST(TestCaseName, TestName) {
 
+class MockFlashMemoryDevice : public FlashMemoryDevice {
+public:
+	// FlashMemoryDevice을(를) 통해 상속됨
+	//unsigned char read(long address) override
+	//{
+	//	return 0;
+	//}
+	void write(long address, unsigned char data) override
+	{
+	}
+
+	MOCK_METHOD(unsigned char, read, (long address), (override));
+};
+
+TEST(FlashMemoryTest, ReadTest) {
+	MockFlashMemoryDevice mDevice;
+
+	EXPECT_CALL(mDevice, read(0))
+		.Times(5);
+
+	DeviceDriver d(&mDevice);
+	d.read(0);
 }
