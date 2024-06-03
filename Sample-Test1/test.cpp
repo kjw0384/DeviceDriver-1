714833d883b723d29f5dbfa7a00f5be074de1638
DESKTOP-5FBE16T\User <kjw0384@gmail.com>
2024-06-03 10:52:43 +0900

    Test Case Refactor 반영 - Fixture 반영

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -11,47 +11,41 @@ public:
 	MOCK_METHOD(void, write, (long, unsigned char), (override));
 };
 
-TEST(DeviceDriverTest, Read5Test) {
+class DeviceDriverFixture : public testing::Test {
+public:
 	MockFlashMemoryDevice mDevice;
+	DeviceDriver driver{ &mDevice };
+};
 
+TEST_F(DeviceDriverFixture, Read5Test) {
 	EXPECT_CALL(mDevice, read(0))
 		.Times(5);
 
-	DeviceDriver driver(&mDevice);
 	driver.read(0);
 }
 
-TEST(DeviceDriverTest, ReadFail) {
-	MockFlashMemoryDevice mDevice;
-
+TEST_F(DeviceDriverFixture, ReadFail) {
 	EXPECT_CALL(mDevice, read(0))
 		.WillOnce(Return(0))
 		.WillOnce(Return(0))
 		.WillOnce(Return(1))
 		.WillRepeatedly(Return(0));
 
-	DeviceDriver driver(&mDevice);
 	EXPECT_THROW(driver.read(0), ReadFailException);
 }
 
-TEST(DeviceDriverTest, WriteCheckCurrentWritten) {
-	MockFlashMemoryDevice mDevice;
-
+TEST_F(DeviceDriverFixture, WriteCheckCurrentWritten) {
 	EXPECT_CALL(mDevice, read(0))
 		.WillRepeatedly(Return(0));
 
-	DeviceDriver driver(&mDevice);
 	EXPECT_THROW(driver.write(0, 'A'), WriteFailException);
 }
 
-TEST(DeviceDriverTest, WriteNormal) {
-	MockFlashMemoryDevice mDevice;
-
+TEST_F(DeviceDriverFixture, WriteNormal) {
 	EXPECT_CALL(mDevice, read(0))
 		.WillRepeatedly(Return(0xFF));
 	EXPECT_CALL(mDevice, write)
 		.Times(1);
 
-	DeviceDriver driver(&mDevice);
 	driver.write(0, 'A');
 }
